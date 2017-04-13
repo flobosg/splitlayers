d286645078f083c596d63adb86962b49c3796d39
Francisco Lobos <flobosg@gmail.com>
2017-04-14 00:03:30 +0200

    Add --keep argument.

--- splitlayers.py
+++ splitlayers.py
@@ -4,16 +4,17 @@ import argparse
 from lxml import etree
 
 parser = argparse.ArgumentParser()
-parser.add_argument("SVG", help="input SVG file")
-parser.add_argument("-p",  metavar='PREFIX', help="set ordered output prefix")
+parser.add_argument("-k",  "--keep", metavar='LAYER', help="keep layer in output files")
+parser.add_argument("-p",  "--prefix", metavar='PREFIX', help="set ordered output prefix")
+parser.add_argument("SVGFILE", help="input SVG file")
 args = parser.parse_args()
 
-TREE = etree.parse(args.SVG)
+TREE = etree.parse(args.SVGFILE)
 ROOT = TREE.getroot()
 LAYER_ATTR = "{http://www.w3.org/2000/svg}g"
 LABEL_ATTR = "{http://www.inkscape.org/namespaces/inkscape}label"
 
-LAYERS = [g for g in ROOT.findall(LAYER_ATTR) if g.get(LABEL_ATTR) != "background"]
+LAYERS = [g for g in ROOT.findall(LAYER_ATTR) if g.get(LABEL_ATTR) != args.keep]
 print len(LAYERS), "layers found."
 
 for i, layer in enumerate(LAYERS, 1):
@@ -25,10 +26,10 @@ for i, layer in enumerate(LAYERS, 1):
     layer_tree = copy.deepcopy(TREE)
     layer_root = layer_tree.getroot()
     for g in layer_root.findall(LAYER_ATTR):
-        if g.get(LABEL_ATTR) not in (layer_title, "background"):
+        if g.get(LABEL_ATTR) not in (args.keep, layer_title):
             layer_root.remove(g)
-    if args.p:
-        out_file = "%s-%s.svg" % (args.p, layer_num)
+    if args.prefix:
+        out_file = "%s-%s.svg" % (args.prefix, layer_num)
     else:
         out_file = "%s.svg" % layer_title
     print out_file, "saved."
